32f4e94b2b2fe2a675c5a572765d9a5e97a9d4aa
parth-commits <parthpatel1087@gmail.com>
2021-12-29 22:08:33 -0500

    update

--- src/components/TimelineView/TimelineItems.js
+++ src/components/TimelineView/TimelineItems.js
@@ -5,9 +5,9 @@ export const TimelineItems = [
         time: 'May 2021 - Present',
         isExpanded: true,
         descriptionItems: [
-            'Actively involved in the development and maintenance of the core SDK, expanding it with features to provide a better User Experience.',
-            'Solved critical errors for multiple products (e.g., Virtual Try On SDK) by creating automated tests, fixing bugs, and improving existing code base with enhanced programming principles which provided an error-free experience to <b>millions</b> of customers of top makeup brands worldwide including but not limited to L’Oréal, Maybelline and NYX.',
-            'Ardent team player, emphasizing frequent code reviews and peer programming sessions.',
+            'Developed and upgraded the core SDK in a team of 7, expanding it with features to provide a better User Experience',
+            '<b>Solved critical errors</b> for multiple SDKs by designing automated tests and fixing bugs which provided an error-free experience to <b>millions</b> of customers of top makeup brands worldwide including L’Oréal, Maybelline and NYX.',
+            'Principles oriented developer emphasizing frequent code reviews and peer programming sessions to improve code quality by 50%.',
             'Experience with JavaScript, Vue.js framework and Python.',
         ],
     },
@@ -17,10 +17,11 @@ export const TimelineItems = [
         time: 'June 2021 - Present',
         isExpanded: true,
         descriptionItems: [
-            'Followed the Software Development Process model to create and maintain multiple apps.',
-            'Created multiple apps that aid in making the user’s daily life activities more convenient. For example, made a <a href="https://galaxy.store/starbucks" target="_blank"> <b> Starbucks Rewards App</b></a> - with currently over <b class="confetti-items">9000</b> users - which allows them to scan their Rewards Card right from their watch, so they don’t have to pull out their phone at the Café. Further optimized the user flow by creating a widget that the user can add to the home screen of their watch to improve time taken to scan their Rewards code by <b>90%</b>. You can checkout the widget version of app <a href="https://galaxy.store/widgetsb" target="_blank"> <b> here</b></a>.',
+            'Followed the Software Development Process model to create and maintain multiple apps improving workflow efficiency by 80%.',
+            'Developed 8 apps that aid in making the user’s daily life activities more convenient. For example, built a <a href="https://galaxy.store/starbucks" target="_blank"><b>Starbucks Rewards App</b></a> - with currently over <b class="confetti-items">10,000</b> users - which allows them to use the Rewards Card right from the smartwatch.',
+            'Further optimized the flow by developing a widget that the user can add to the home screen of the smartwatch to improve time to scan the Rewards code by 90%',
             'Other interesting apps include: <a href="https://galaxy.store/sqadoxczl" target="_blank"> <b> SB Scan + Pay</b></a>, <a href="https://galaxy.store/tims1" target="_blank"><b>Tims Rewards</b></a>, <a href="https://galaxy.store/tic1" target="_blank"><b>TicTacToe</b></a>, <a href="https://galaxy.store/sudoku3" target="_blank"><b>Sudoku</b></a>.',
-            'Total Downloads from all apps: <b class="confetti-items">10,000+</b>',
+            'Total Downloads from all apps: <b class="confetti-items">11,000+</b>',
         ],
     },
     {
